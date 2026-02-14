10ead113a100cbabc100336f989bcaf8d6679066
bartvanhoey <bartvanhoey@hotmail.com>
2026-02-14 23:20:31 +0100

    update

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,9 +1,10 @@
+import NotLoggedInUserOnly from "@/components/auth/NotLoggedInUserOnly";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 export default function AuthLayout() {
   return (
-    <>
+    <NotLoggedInUserOnly>
       <StatusBar style="auto" />
       <Stack
         screenOptions={{ headerShown: false, animation: "slide_from_bottom" }}
@@ -11,6 +12,7 @@ export default function AuthLayout() {
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="register" options={{ headerShown: false }} />
       </Stack>
-    </>
+    </NotLoggedInUserOnly>
+    
   );
 }
